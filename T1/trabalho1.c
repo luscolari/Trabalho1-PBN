2d8952c7ce95e3c74bf3014be7bb8e64a7180d5c
luscolari <141656103+luscolari@users.noreply.github.com>
2024-04-25 10:22:34 -0300

    Update trabalho1.c

--- T1/trabalho1.c
+++ T1/trabalho1.c
@@ -3,60 +3,22 @@
 
 
 
-void imagemCinza(FILE) {
-    //matriz[j][i] = (r*0.30)+(g*0.59)+(b*0.11);
-
-
-}
-
-void imagemEnvelhecida(FILE) {
-//Subtrair cada pixel de 20
-
-}
-
-void imagemNegativa(FILE) {
-     //Subtrair cada pixel de 255
-//Ex.: red = 130
-
-}
-
-void imagemBrilho(FILE) {
-//Multiplicar cada pixel por 1.3, limitando a 255.
-
-}
-
-void imagemEscura(FILE) {
-   
-
-
-}
-
-void imagemRotacionada(FILE) {
-
-}
-
 typedef struct  
 {
     int R, G, B;
 }Pixel;
 
 
-int main(int){
+int main(void){
     FILE *fp;
     int c;
     char extensaoIMG[3];
     int i, j, linhas, colunas, valor, r, g, b;
-    struct Pixel **matriz = (struct Pixel **)malloc(linhas * sizeof(struct Pixel *));
-    for (int i = 0; i < linhas; i++) {
-    matriz[i] = (struct Pixel *)malloc(colunas * sizeof(struct Pixel));
-    }
 
 
     printf("---- BEM VINDO -----");
     printf("Por favor digite o nome da imagem: ");
    //scanf();
-
-   FILE * fp;
 
    fp = fopen ("kyle.ppm", "r");
 
@@ -64,11 +26,17 @@ int main(int){
     fscanf(fp, "%d %d", &colunas, &linhas); 	// lê o tamanho da matriz  
     fscanf(fp, "%d", &valor); 					// lê o valor máximo. 
     
+    
+    Pixel **matriz = (Pixel **)malloc(linhas * sizeof(Pixel *));
 
-    for (i = 0; i < linhas; i++) 
-    {
-        for (j = 0; j < colunas; j++) 
-        {
+
+    for (int i = 0; i < linhas; i++) {
+        matriz[i] = (Pixel *)malloc(colunas * sizeof(Pixel));
+
+    }
+
+    for (int i = 0; i < linhas; i++) {
+        for (int j = 0; j < colunas; j++) {
             fscanf(fp, "%d %d %d", &r, &g, &b);
             matriz[i][j].R = r;
             matriz[i][j].G = g;
@@ -76,6 +44,17 @@ int main(int){
         }
     }
 
+    for (int i = 0; i < linhas; i++) {
+        for (int j = 0; j < colunas; j++) {
+            printf("%d", matriz[i][j]);
+        }
+    }
 
-    return 0;
+     for (int i = 0; i < linhas; i++) {
+        free(matriz[i]);
+    }
+    free(matriz);
+
+    fclose(fp);
+
 }
